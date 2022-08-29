b7ff1d81f13c2e0bb9f447b511e198665bcd4d0d
Jagdeep Singh Bisht <jagdeepbisht89@gmail.com>
2022-08-30 01:13:54 +0530

    Added public and protected routes with middleware hoc

--- components/AuthenticationChecker.tsx
+++ components/AuthenticationChecker.tsx
@@ -1,11 +1,21 @@
 import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
 import { Props } from '../types/Props'
 import { SignIn } from './SignIn'
 
 function AuthenticationChecker({ children }: Props) {
+  const router = useRouter()
   const { data: session, status } = useSession()
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null)
 
-  if (session && status === 'authenticated') {
+  useEffect(() => {
+    setIsAuthenticated(status === 'authenticated')
+  }, [router, status])
+
+  if (isAuthenticated === null) return null
+
+  if (isAuthenticated) {
     return <p>{children}</p>
   }
 
